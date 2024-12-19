11b379b008d95c7b756ac98fafbeb38595a5d242
Vaibhav Dhole <vaibhav123dhole@gmail.com>
2024-12-19 21:28:57 +0530

    minor changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7RTMG6GV.mjs";import"./chunk-NBXFG73P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IMO6FSVD.mjs";import"./chunk-NBXFG73P.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
